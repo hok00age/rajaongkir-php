c51be51c0fe320f524044be956312cc17d34267d
Damar Riyadi <damar@tahutek.net>
2015-01-14 18:08:54 +0700

    Added code comment

--- src/RajaOngkir.php
+++ src/RajaOngkir.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * RajaOngkir PHP Client
+ * 
+ * Class PHP untuk mengkonsumsi API RajaOngkir
+ * Berdasarkan dokumentasi RajaOngkir http://rajaongkir.com/dokumentasi
+ * 
+ * @author Damar Riyadi <damar@tahutek.net>
+ */
+
 namespace hok00age;
 
 class RajaOngkir {
@@ -7,6 +16,11 @@ class RajaOngkir {
     private static $api_key;
     private static $base_url = "http://rajaongkir.com/api/";
 
+    /**
+     * Constructor
+     * @param string $api_key API Key Anda sebagaimana yang tercantum di akun panel RajaOngkir
+     * @param array $additional_headers Header tambahan seperti android-key, ios-key, dll
+     */
     public function __construct($api_key, $additional_headers = array()) {
         RajaOngkir::$api_key = $api_key;
         \Unirest::defaultHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -16,11 +30,22 @@ class RajaOngkir {
         }
     }
 
+    /**
+     * Fungsi untuk mendapatkan data propinsi di Indonesia
+     * @param integer $province_id ID propinsi, jika NULL tampilkan semua propinsi
+     * @return object Object yang berisi informasi response, terdiri dari: code, headers, body, raw_body.
+     */
     function getProvince($province_id = NULL) {
         $params = (is_null($province_id)) ? NULL : array('id' => $province_id);
         return \Unirest::get(RajaOngkir::$base_url . "province", array(), $params);
     }
 
+    /**
+     * Fungsi untuk mendapatkan data kota di Indonesia
+     * @param integer $province_id ID propinsi
+     * @param integer $city_id ID kota, jika ID propinsi dan kota NULL maka tampilkan semua kota
+     * @return object Object yang berisi informasi response, terdiri dari: code, headers, body, raw_body.
+     */
     function getCity($province_id = NULL, $city_id = NULL) {
         $params = (is_null($province_id)) ? NULL : array('province' => $province_id);
         if (!is_null($city_id)) {
@@ -29,6 +54,14 @@ class RajaOngkir {
         return \Unirest::get(RajaOngkir::$base_url . "city", array(), $params);
     }
 
+    /**
+     * Fungsi untuk mendapatkan data ongkos kirim
+     * @param integer $origin ID kota asal
+     * @param integer $destination ID kota tujuan
+     * @param integer $weight Berat kiriman dalam gram
+     * @param string $courier Kode kurir, jika NULL maka tampilkan semua kurir
+     * @return object Object yang berisi informasi response, terdiri dari: code, headers, body, raw_body.
+     */
     function getCost($origin, $destination, $weight, $courier = NULL) {
         $params = array(
             'origin' => $origin,
