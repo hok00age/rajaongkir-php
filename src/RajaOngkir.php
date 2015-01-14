9fc98eeb40f64c8a2172340089cf1f41cd2002b3
Damar Riyadi <damar@tahutek.net>
2015-01-14 17:16:35 +0700

    Added function getCity dan getCost

--- src/RajaOngkir.php
+++ src/RajaOngkir.php
@@ -7,18 +7,38 @@ class RajaOngkir {
     private static $api_key;
     private static $base_url = "http://rajaongkir.com/api/";
 
-    public function __construct($api_key) {
+    public function __construct($api_key, $additional_headers = array()) {
         RajaOngkir::$api_key = $api_key;
         \Unirest::defaultHeader("Content-Type", "application/x-www-form-urlencoded");
         \Unirest::defaultHeader("key", RajaOngkir::$api_key);
-    }
-
-    function getProvince($province_id = NULL, $additional_headers = array()) {
-        $params = (is_null($province_id)) ? NULL : array('id' => $province_id);
         foreach ($additional_headers as $key => $value) {
             \Unirest::defaultHeader($key, $value);
         }
+    }
+
+    function getProvince($province_id = NULL) {
+        $params = (is_null($province_id)) ? NULL : array('id' => $province_id);
         return \Unirest::get(RajaOngkir::$base_url . "province", array(), $params);
+    }
+
+    function getCity($city_id = NULL, $province_id = NULL) {
+        $params = (is_null($city_id)) ? NULL : array('id' => $city_id);
+        if (!is_null($province_id)) {
+            $params['province'] = $province_id;
+        }
+        return \Unirest::get(RajaOngkir::$base_url . "city", array(), $params);
+    }
+
+    function getCost($origin, $destination, $weight, $courier = NULL) {
+        $params = array(
+            'origin' => $origin,
+            'destination' => $destination,
+            'weight' => $weight
+        );
+        if (!is_null($courier)) {
+            $params['courier'] = $courier;
+        }
+        return \Unirest::post(RajaOngkir::$base_url . "cost", array(), $params);
     }
 
 }
